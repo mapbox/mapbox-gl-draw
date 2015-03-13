f7f80c32e8858f5b4e762990943f2d1223e70d5a
tristen <tristen.brown@gmail.com>
2015-03-13 00:44:42 -0400

    Add control elements to the map

--- src/draw.js
+++ src/draw.js
@@ -16,8 +16,25 @@ Draw.prototype = util.inherit(Control, {
   },
 
   onAdd: function(map) {
-    var className = 'mapboxgl-ctrl-nav';
+    var className = 'mapboxgl-ctrl-nav mapboxgl-ctrl-draw';
     var container = this._container = DOM.create('div', className, map.getContainer());
+
+    this._shapeButton = this._createButton('mapboxgl-ctrl-draw-btn shape', this._drawShape.bind(map));
+    this._circleButton = this._createButton('mapboxgl-ctrl-draw-btn circle', this._drawCircle.bind(map));
+    this._markerButton = this._createButton('mapboxgl-ctrl-draw-btn marker', this._drawMarker.bind(map));
+
     return container;
+  },
+
+  _drawShape: function(map) {},
+
+  _drawCircle: function(map) {},
+
+  _drawMarker: function(map) {},
+
+  _createButton: function(className, fn) {
+    var a = DOM.create('button', className, this._container);
+    a.addEventListener('click', function() { fn(); });
+    return a;
   }
 });
