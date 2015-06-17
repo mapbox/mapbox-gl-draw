5125274a4836127def09978292e4b3291914b27d
jakepruitt <jake@mapbox.com>
2015-06-17 16:25:11 -0400

    Minimize testing setup

--- test/test.js
+++ test/test.js
@@ -3,19 +3,14 @@
 var test = require('tape');
 var mapboxgl = require('mapbox-gl');
 var Draw = require('../src/draw.js');
-var util = require('../src/util.js');
-var DOM = util.DOM;
 
 function createMap() {
-  var body = document.querySelector('body');
-  var mapDiv = DOM.create('div', 'map', body);
+  var mapEl = document.createElement('div');
 
-  mapboxgl.accessToken = 'pk.eyJ1IjoidHJpc3RlbiIsImEiOiJiUzBYOEJzIn0.VyXs9qNWgTfABLzSI3YcrQ';
   return new mapboxgl.Map({
-    container: mapDiv,
-    zoom: 12,
-    center: [43.6579, -79.3712],
-    style: 'https://www.mapbox.com/mapbox-gl-styles/styles/outdoors-v7.json'
+    container: mapEl,
+    interactive: false,
+    attributionControl: false
   });
 }
 
