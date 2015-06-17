59466006277c2854263134c21575a7e3ffa0dab8
jakepruitt <jake@mapbox.com>
2015-06-17 15:44:44 -0400

    Fixed documentation typo

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
 
+var mapboxgl = require('mapbox-gl');
 /** A drawing component for mapboxgl
  * @class mapbox.Draw
  *
  * @param {Object} options
- * @param {String} [options.position=top-right] A string indicating the control's position on the map. Options are `topright`, `topleft`, `bottomright`, `bottomleft`
+ * @param {String} [options.position=top-left] A string indicating the control's position on the map. Options are `topright`, `topleft`, `bottomright`, `bottomleft`
  * @returns {Draw} `this`
  * @example
  * var map = new mapboxgl.Map({
