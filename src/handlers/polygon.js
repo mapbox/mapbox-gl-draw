2d44d0bd7cd5194e1e67c4c56389c8b963e976ec
kelvinabrokwa <kelvinabrokwa@gmail.com>
2015-07-23 19:17:41 -0400

    start geometry edit

--- src/handlers/polygon.js
+++ src/handlers/polygon.js
@@ -3,6 +3,7 @@
 var Immutable = require('immutable');
 var xtend = require('xtend');
 var Handler = require('./handlers');
+var { translatePoint } = require('../util');
 
 /**
  * Polygon geometry object
@@ -62,6 +63,42 @@ Polygon.prototype = xtend(Handler, {
     this._map.getContainer().classList.remove('mapboxgl-draw-activated');
 
     this._done('polygon');
+  },
+
+  moveVertex(init, curr, vertex) {
+    if (!this.movingVertex) {
+      var coords = vertex.geometry.coordinates;
+      var diff = Infinity;
+      /* if only we could do this...sigh
+      this.vertexIdx = this.feature.getIn(['geometry', 'coordinates', 0]).findIndex(v =>
+        (v.get(0) === coords[0]) && (v.get(1) === coords[1])
+      );
+      */
+      this.feature.getIn(['geometry', 'coordinates', 0]).forEach((v, i) => {
+        //var d = Math.sqrt(Math.pow(v.getIn([i, 0]) - coords[0], 2) + Math.pow(v.getIn([i, 1]) - coords[1], 2));
+        var d = Math.abs(v.get(0) - coords[0]);
+        if (d < diff) {
+          this.vertexIdx = i;
+          diff = d;
+        }
+      });
+      /*
+      var c = this.feature.getIn(['geometry', 'coordinates', 0]);
+      for (var i = 0; i < c; i++) {
+        var d = Math.sqrt(Math.pow(c[i][0] - coords[0], 2) + Math.pow(c[i][1] - coords[1], 2));
+        if (d < diff) {
+          diff = d;
+        }
+      }
+      */
+      this.initCoords = this.feature.getIn(['geometry', 'coordinates', 0, this.vertexIdx]);
+      this.movingVertex = true;
+    }
+    var dx = curr.x - init.x;
+    var dy = curr.y - init.y;
+    var newPoint = translatePoint(this.initCoords.toJS(), dx, dy, this._map);
+    this.feature = this.feature.setIn(['geometry', 'coordinates', 0, this.vertexIdx], newPoint);
+    this.store.update(this.feature.toJS());
   }
 
 });
