315bdbcf46158f5663bfc1cff96e40d16b84cf4b
kelvinabrokwa <kelvinabrokwa@gmail.com>
2015-07-24 01:10:36 -0400

    implement for square

--- src/handlers/line.js
+++ src/handlers/line.js
@@ -69,7 +69,7 @@ Line.prototype = xtend(Handler, {
       var diff = Infinity;
 
       this.feature.getIn(['geometry', 'coordinates']).forEach((v, i) => {
-        //var d = Math.sqrt(Math.pow(v.get(0) - coords[0], 2) + Math.pow(v.get(1) - coords[1], 2));
+        //var d = Math.sqrt(Math.pow(v.get(0) - coords[1], 2) + Math.pow(v.get(1) - coords[0], 2));
         var d = Math.abs(v.get(0) - coords[0]);
         if (d < diff) {
           this.vertexIdx = i;
