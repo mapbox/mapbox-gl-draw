e0e489f03ff0dcd20b7733248f166f91e09e8fee
kelvinabrokwa <kelvinabrokwa@gmail.com>
2015-07-24 00:14:50 -0400

    implement for linestrings

--- src/handlers/line.js
+++ src/handlers/line.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var xtend = require('xtend');
+var Immutable = require('immutable');
 var Handler = require('./handlers');
+var { translatePoint } = require('../util');
 
 /**
  * @param {Object} map - Instance of MapboxGL Map
@@ -57,6 +59,33 @@ Line.prototype = xtend(Handler, {
     this.coodinates = this.coordinates.splice(-1, 1);
 
     this._done('line');
+  },
+
+  moveVertex(init, curr, vertex) {
+    if (!this.movingVertex) {
+      this.movingVertex = true;
+
+      var coords = vertex.geometry.coordinates;
+      var diff = Infinity;
+
+      this.feature.getIn(['geometry', 'coordinates']).forEach((v, i) => {
+        //var d = Math.sqrt(Math.pow(v.get(0) - coords[0], 2) + Math.pow(v.get(1) - coords[1], 2));
+        var d = Math.abs(v.get(0) - coords[0]);
+        if (d < diff) {
+          this.vertexIdx = i;
+          diff = d;
+        }
+      });
+      this.initCoords = this.feature.getIn(['geometry', 'coordinates', this.vertexIdx]);
+    }
+
+    var dx = curr.x - init.x;
+    var dy = curr.y - init.y;
+    var newPoint = translatePoint(this.initCoords.toJS(), dx, dy, this._map);
+
+    this.feature = this.feature.setIn(['geometry', 'coordinates', this.vertexIdx], Immutable.fromJS(newPoint));
+
+    this.store.update(this.feature.toJS());
   }
 
 });
