fb4a531b6f9e4477efa20b8dfced8ed329d2e905
kelvinabrokwa <kelvinabrokwa@gmail.com>
2016-02-08 18:59:03 -0500

    remove examples

--- build-examples.sh
+++
@@ -1,7 +0,0 @@
-for example in examples/*; do
-  if [ -d $example ]; then
-    browserify $example/index.js > $example/bundle.js;
-    #cp dist/mapbox* $example;
-    #cp -rf dist/font $example;
-  fi
-done;
