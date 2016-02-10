94a4134ce385e87e3e1eeb1050bb620c3f18c41d
Drew Bollinger <drew@developmentseed.org>
2016-02-09 20:51:11 -0500

    Add additional test for safety

--- test/draw.test.js
+++ test/draw.test.js
@@ -62,6 +62,9 @@ map.on('load', () => {
       drawing: false
     });
 
+    t.doesNotThrow(Draw._showDeleteButton.bind(Draw),
+    'calling _showDeleteButton with drawing disabled does not throw an error');
+
     t.doesNotThrow(Draw._hideDeleteButton.bind(Draw),
     'calling _hideDeleteButton with drawing disabled does not throw an error');
 
