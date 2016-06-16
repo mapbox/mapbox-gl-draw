259e76cd73938d7b1e1e3e86fe74f82957560cfd
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2016-06-16 13:52:17 -0400

    add annotations to the event tests

--- test/events.test.js
+++ test/events.test.js
@@ -30,10 +30,10 @@ test('draw.modified is fired when placing a point', t => {
       lngLat: {lng: 0, lat: 0}
   });
 
-  t.equal('mousedown', map.fire.getCall(0).args[0]);
-  t.equal('mouseup', map.fire.getCall(1).args[0]);
-  t.equal('draw.modechange', map.fire.getCall(2).args[0]);
-  t.equal('draw.modified', map.fire.getCall(3).args[0]);
+  t.equal('mousedown', map.fire.getCall(0).args[0], 'mousedown was emitted');
+  t.equal('mouseup', map.fire.getCall(1).args[0], 'moseup was emitted');
+  t.equal('draw.modechange', map.fire.getCall(2).args[0], 'change mode was emitted');
+  t.equal('draw.modified', map.fire.getCall(3).args[0], 'draw.modified was emitted');
   t.end();
 });
 
@@ -68,20 +68,24 @@ test('draw.modified is fired when drawing a line_string', t => {
       lngLat: {lng: 0, lat: 5}
   });
 
-  var events = [
-    'mousedown',
-    'mouseup',
-    'mousedown',
-    'mouseup',
-    'draw.modified',
-    'mousedown',
-    'mouseup',
-    'draw.modified'
-  ];
 
-  events.forEach((event, i) => {
-    var args = map.fire.getCall(i).args;
-    t.equal(event, args[0]);
+  t.test('draw.modified should not be called when the feature is invalid', t => {
+    var events = [
+      'mousedown',
+      'mouseup',
+      'mousedown',
+      'mouseup',
+      'draw.modified',
+      'mousedown',
+      'mouseup',
+      'draw.modified'
+    ];
+
+    events.forEach((event, i) => {
+      var args = map.fire.getCall(i).args;
+      t.equal(event, args[0], `${event} was called ${i+1}`);
+    });
+    t.end();
   });
 
   t.end();
@@ -127,22 +131,25 @@ test('draw.modified is fired when making a polygon', t => {
       lngLat: {lng: 5, lat: 5}
   });
 
-  var events = [
-    'mousedown',
-    'mouseup',
-    'mousedown',
-    'mouseup',
-    'mousedown',
-    'mouseup',
-    'draw.modified',
-    'mousedown',
-    'mouseup',
-    'draw.modified'
-  ];
+  t.test('draw.modified should not be called when the feature is invalid', t => {
+    var events = [
+      'mousedown',
+      'mouseup',
+      'mousedown',
+      'mouseup',
+      'mousedown',
+      'mouseup',
+      'draw.modified',
+      'mousedown',
+      'mouseup',
+      'draw.modified'
+    ];
 
-  events.forEach((event, i) => {
-    var args = map.fire.getCall(i).args;
-    t.equal(event, args[0]);
+    events.forEach((event, i) => {
+      var args = map.fire.getCall(i).args;
+      t.equal(event, args[0], `${event} was called ${i+1}`);
+    });
+    t.end();
   });
 
   t.end();
@@ -161,15 +168,15 @@ test('draw.deleted event', t => {
   Draw.delete(id);
   setTimeout(function() {
     var event = map.fire.getCall(0).args;
-    t.equal('draw.deleted', event[0]);
-    t.equal(1, event[1].featureIds.length);
+    t.equal('draw.deleted', event[0], 'deleted was emitted');
+    t.equal(1, event[1].featureIds.length, 'the right number of features were deleted');
     // this event is weird, need to fix
     //t.equal(id, event[1].featureIds[0]);
 
     let numCalls = map.fire.callCount;
     for(var i=0; i<numCalls; i++) {
       var args = map.fire.getCall(i).args;
-      t.notEqual('draw.modified', args[0]);
+      t.notEqual('draw.modified', args[0], 'draw.modified was never called');
     }
 
     Draw.remove();
