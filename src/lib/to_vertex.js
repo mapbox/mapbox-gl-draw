577b93be58d4a29d483f1ecf7c65dac7eb388f05
DAVID CLARK <dclark@mapbox.com>
2016-06-17 15:45:45 -0700

    toVertex tests

--- src/lib/to_vertex.js
+++ src/lib/to_vertex.js
@@ -1,16 +1,26 @@
-
-module.exports = function(parent, coord, path, selected) {
+/**
+ * Returns GeoJSON for a Point representing the
+ * vertex of another feature.
+ *
+ * @param {string} parentId
+ * @param {Array<number>} coordinates
+ * @param {string} path - Dot-separated numbers indicating exactly
+ *   where the point exists within its parent feature's coordinates.
+ * @param {boolean} selected
+ * @return {GeoJSON} Point
+ */
+module.exports = function(parentId, coordinates, path, selected) {
   return {
-      type: 'Feature',
-      properties: {
-        meta: 'vertex',
-        parent: parent,
-        coord_path: path,
-        active: `${selected}`
-      },
-      geometry: {
-        type: 'Point',
-        coordinates: coord
-      }
-    };
+    type: 'Feature',
+    properties: {
+      meta: 'vertex',
+      parent: parentId,
+      coord_path: path,
+      active: `${selected}`
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: coordinates
+    }
+  };
 };
