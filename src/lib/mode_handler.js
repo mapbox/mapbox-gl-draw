68b1d4524727d123e340fda2e11bd993b2ff221f
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2016-11-07 18:26:23 -0500

    remove truncation from draw

--- src/lib/mode_handler.js
+++ src/lib/mode_handler.js
@@ -1,4 +1,3 @@
-const truncatePoint = require('./truncate_point');
 
 var ModeHandler = function(mode, DrawContext) {
 
@@ -30,7 +29,7 @@ var ModeHandler = function(mode, DrawContext) {
 
   var delegate = function (eventName, event) {
     if (event.lngLat) {
-      event.lngLat = truncatePoint(event.lngLat);
+      event.lngLat = event.lngLat;
     }
     var handles = handlers[eventName];
     var iHandle = handles.length;
