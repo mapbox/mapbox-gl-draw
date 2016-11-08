e902343dd151d4663fd554a3f079a36b0f74b6f5
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2016-11-08 10:29:57 -0500

    remove noop if block

--- src/lib/mode_handler.js
+++ src/lib/mode_handler.js
@@ -28,9 +28,6 @@ var ModeHandler = function(mode, DrawContext) {
   };
 
   var delegate = function (eventName, event) {
-    if (event.lngLat) {
-      event.lngLat = event.lngLat;
-    }
     var handles = handlers[eventName];
     var iHandle = handles.length;
     while (iHandle--) {
