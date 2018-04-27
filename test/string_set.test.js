dc675c098f1ed7daa970251aa8fa9f765053d6c3
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2018-04-27 10:38:12 -0400

    allow features to have numeric ids

--- test/string_set.test.js
+++ test/string_set.test.js
@@ -13,8 +13,8 @@ test('StringSet constructor and API', t => {
   t.equal(typeof StringSet.prototype.clear, 'function', 'exposes set.clear');
   t.equal(Object.keys(StringSet.prototype).filter(k => k[0] !== '_').length, 5, 'no unexpected methods');
 
-  const populatedSet = new StringSet(['a', 'b']);
-  t.deepEqual(populatedSet.values(), ['a', 'b'], 'populated by constructor arg');
+  const populatedSet = new StringSet(['a', 4, 'b']);
+  t.deepEqual(populatedSet.values(), ['a', 4, 'b'], 'populated by constructor arg');
 
   t.end();
 });
@@ -28,29 +28,35 @@ test('StringSet#add', t => {
   t.deepEqual(set.values(), ['a', 'b']);
   set.add('a');
   t.deepEqual(set.values(), ['a', 'b']);
+  set.add(3);
+  t.deepEqual(set.values(), ['a', 'b', 3]);
   t.end();
 });
 
 test('StringSet#delete', t => {
-  const subject = ['a', 'b'];
+  const subject = ['a', 'b', 2];
   const set = new StringSet(subject);
   set.delete('a');
-  t.deepEqual(set.values(), ['b']);
+  t.deepEqual(set.values(), ['b', 2]);
   set.delete('a');
-  t.deepEqual(set.values(), ['b']);
+  t.deepEqual(set.values(), ['b', 2]);
   set.delete();
-  t.deepEqual(set.values(), ['b']);
+  t.deepEqual(set.values(), ['b', 2]);
   set.delete('b');
+  t.deepEqual(set.values(), [2]);
+  set.delete(2);
   t.deepEqual(set.values(), []);
-  t.deepEqual(subject, ['a', 'b'], 'source array not mutated');
+  t.deepEqual(subject, ['a', 'b', 2], 'source array not mutated');
   t.end();
 });
 
 test('StringSet#has', t => {
-  const set = new StringSet(['a', 'b']);
+  const set = new StringSet(['a', 'b', 2]);
   t.equal(set.has('a'), true);
   t.equal(set.has('b'), true);
+  t.equal(set.has(2), true);
   t.equal(set.has('c'), false);
+  t.equal(set.has(4), false);
   t.end();
 });
 
