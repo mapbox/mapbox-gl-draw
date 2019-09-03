71a94cf12a50f8352656db0fa6ea89dcbfd26de6
igorkostyrko <igor.kostyrko@altusnova.com>
2019-09-03 11:33:02 +0300

    perf(mode_handler): CPU usage
    Fix high CPU usage on mousemove by adding throttling to drag
    and mousemove events, added requestAnimationFrame

--- src/lib/mode_handler.js
+++ src/lib/mode_handler.js
@@ -1,3 +1,34 @@
+function throttle(func, wait, options) {
+  var context, args, result;
+  var timeout = null;
+  var previous = 0;
+  if (!options) options = {};
+  var later = function() {
+    previous = options.leading === false ? 0 : Date.now();
+    timeout = null;
+    result = func.apply(context, args);
+    if (!timeout) context = args = null;
+  };
+  return function() {
+    var now = Date.now();
+    if (!previous && options.leading === false) previous = now;
+    var remaining = wait - (now - previous);
+    context = this;
+    args = arguments;
+    if (remaining <= 0 || remaining > wait) {
+      if (timeout) {
+        clearTimeout(timeout);
+        timeout = null;
+      }
+      previous = now;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null;
+    } else if (!timeout && options.trailing !== false) {
+      timeout = setTimeout(later, remaining);
+    }
+    return result;
+  };
+}
 
 const ModeHandler = function(mode, DrawContext) {
 
@@ -38,8 +69,21 @@ const ModeHandler = function(mode, DrawContext) {
       const handle = handles[iHandle];
       if (handle.selector(event)) {
         handle.fn.call(ctx, event);
-        DrawContext.store.render();
-        DrawContext.ui.updateMapClasses();
+
+        if (
+          DrawContext.api.getMode() === 'simple_select'
+          && (
+            eventName === 'mousemove'
+            || eventName === 'drag' && !DrawContext.api.getSelectedIds().length
+          )
+        ) {
+          return;
+        }
+
+        window.requestAnimationFrame(() => {
+          DrawContext.store.render();
+          DrawContext.ui.updateMapClasses();
+        });
 
         // ensure an event is only handled once
         // we do this to let modes have multiple overlapping selectors
@@ -72,15 +116,15 @@ const ModeHandler = function(mode, DrawContext) {
         mode.uncombineFeatures();
       }
     },
-    drag: function(event) {
+    drag: throttle(function(event) {
       delegate('drag', event);
-    },
+    }, 16.6),
     click: function(event) {
       delegate('click', event);
     },
-    mousemove: function(event) {
+    mousemove: throttle(function(event) {
       delegate('mousemove', event);
-    },
+    }, 16.6),
     mousedown: function(event) {
       delegate('mousedown', event);
     },
