e0979e36d0497199a3735bb48b7a996d7be990f1
Vesa Hautsalo <vesa@fastmail.fm>
2020-12-02 13:30:41 +0200

    Readd source check to render()

--- src/render.js
+++ src/render.js
@@ -3,7 +3,8 @@ import * as Constants from './constants';
 export default function render() {
   // eslint-disable-next-line no-invalid-this
   const store = this;
-  if (!store.ctx.map) return cleanup();
+  const mapExists = store.ctx.map && store.ctx.map.getSource(Constants.SOURCE) !== undefined;
+  if (!mapExists) return cleanup();
 
   const mode = store.ctx.events.currentModeName();
 
