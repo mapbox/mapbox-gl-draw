69568a7ef94f0769de33dc35a61c0ed87216e900
Vladimir Agafonkin <agafonkin@gmail.com>
2022-08-30 23:03:30 +0300

    get rid of remove-flow-types & uglify-js leftovers

--- bench/rollup.config.js
+++ bench/rollup.config.js
@@ -1,22 +1,9 @@
 
-import flowRemoveTypes from '@mapbox/flow-remove-types';
 import replace from '@rollup/plugin-replace';
 import json from '@rollup/plugin-json';
 import buble from '@rollup/plugin-buble';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-
-// Using this instead of rollup-plugin-flow due to
-// https://github.com/leebyron/rollup-plugin-flow/issues/5
-function flow() {
-  return {
-    name: 'flow-remove-types',
-    transform: code => ({
-      code: flowRemoveTypes(code).toString(),
-      map: null
-    })
-  };
-}
 
 export default {
   input: ['bench/index.js'],
@@ -34,14 +21,12 @@ export default {
     'path'
   ],
   plugins: [
-    flow(),
     json(),
     replace({
       'process.env.MapboxAccessToken': JSON.stringify(process.env.MapboxAccessToken),
       'process.env.MAPBOX_ACCESS_TOKEN': JSON.stringify(process.env.MAPBOX_ACCESS_TOKEN),
     }),
     buble({transforms: {dangerousForOf: true}, objectAssign: "Object.assign"}),
-    false,
     resolve({
       browser: true,
       preferBuiltins: false
