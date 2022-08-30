440c97eb607686723c338770e3b41631d0c94404
Vladimir Agafonkin <agafonkin@gmail.com>
2022-08-30 16:38:07 +0300

    remove unused remnants of browserify

--- server.js
+++ server.js
@@ -1,21 +1,5 @@
 var express = require('express');
 var app = express();
-var browserify = require('browserify-middleware');
-
-
-app.get('/mapbox-gl-draw.js', browserify('./index.js', {
-    standalone: 'MapboxDraw',
-    debug: true,
-    cache: 'dynamic',
-    minify: false
-}));
-
-app.get('/app.js', browserify('./debug/app.js', {
-    standalone: 'MapboxDraw',
-    debug: true,
-    cache: 'dynamic',
-    minify: false
-}));
 
 app.get('/mapbox-gl.js', function(req, res) {
   res.sendFile(__dirname+'/node_modules/mapbox-gl/dist/mapbox-gl.js');
@@ -24,13 +8,6 @@ app.get('/mapbox-gl.js', function(req, res) {
 app.get('/mapbox-gl.css', function(req, res) {
     res.sendFile(__dirname+ '/node_modules/mapbox-gl/dist/mapbox-gl.css');
 });
-
-app.get('/bench/index.js', browserify('./bench/index.js', {
-    transform: ['unassertify', 'envify'],
-    debug: true,
-    minify: true,
-    cache: 'dynamic'
-}));
 
 app.get('/bench/:name', function(req, res) {
     res.sendFile(__dirname + '/bench/index.html');
