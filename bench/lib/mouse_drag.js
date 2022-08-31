38c0fac87f54b81d039bc17d8438eca32f094c64
Vladimir Agafonkin <agafonkin@gmail.com>
2022-08-31 18:08:24 +0300

    lint the bench folder too

--- bench/lib/mouse_drag.js
+++ bench/lib/mouse_drag.js
@@ -1,21 +1,20 @@
-import mouseEvents from './mouse_events'
-import mousePath from './mouse_path'
+import mouseEvents from './mouse_events';
+import mousePath from './mouse_path';
 
 export default function(start, map) {
 
-  var path = mousePath(start);
-
-  var events = mouseEvents(map);
+  const path = mousePath(start);
+  const events = mouseEvents(map);
 
   events.push('mousedown', {
     x: start.x,
     y: start.y
   }, true);
 
-  for (var i=0; i<path.length; i++) {
+  for (let i = 0; i < path.length; i++) {
     events.push('mousemove', path[i]);
   }
-  for (var i=path.length-1; i>=0; i--) {
+  for (let i = path.length - 1; i >= 0; i--) {
     events.push('mousemove', path[i]);
   }
 
@@ -26,5 +25,5 @@ export default function(start, map) {
 
   return function(cb) {
     events.run(cb);
-  }
+  };
 }
