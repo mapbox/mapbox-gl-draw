3320981f572d793b10d2c3d9bc3a52c64cad28ed
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2024-06-27 16:01:39 +0300

    fix tests

--- src/options.js
+++ src/options.js
@@ -38,7 +38,7 @@ const hideControls = {
 function addSources(styles, sourceBucket) {
   return styles.map((style) => {
     if (style.source) return style;
-    return Object.assign(style, {
+    return Object.assign({}, style, {
       id: `${style.id}.${sourceBucket}`,
       source: (sourceBucket === 'hot') ? Constants.sources.HOT : Constants.sources.COLD
     });
