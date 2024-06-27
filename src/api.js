bd6f2c36de8821477d220b8c7cb7069302e2b9fb
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2024-06-27 16:07:33 +0300

    use fast-deep-equal instead of lodash.isequal

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-import isEqual from 'lodash.isequal';
+import isEqual from 'fast-deep-equal';
 import normalize from '@mapbox/geojson-normalize';
 import hat from 'hat';
 import featuresAt from './lib/features_at';
