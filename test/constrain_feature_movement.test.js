1c59954a384f62d1d1942bd70097a3c4545e3a4c
Steve Bennett <me@stevebennett.me>
2024-06-29 00:37:52 +1000

    Remove geojson-flatten dependency (#1066)
    * Fix geometry depth of polygon tests.
    
    * Remove geojson-extent dependency.
    
    ---------
    
    Co-authored-by: Steve Bennett <steveben@fb.com>
    Co-authored-by: Stepan Kuzmin <stepan.kuzmin@mapbox.com>

--- test/constrain_feature_movement.test.js
+++ test/constrain_feature_movement.test.js
@@ -173,7 +173,7 @@ test('constrainFeatureMovement line, requiring eastern wrap', () => {
 
 test('constrainFeatureMovement polygon, no constraint', () => {
   const polygon = getGeoJSON('polygon');
-  polygon.geometry.coordinates = [[0, 0], [10, 10], [20, 20], [0, 0]];
+  polygon.geometry.coordinates = [[[0, 0], [10, 10], [20, 20], [0, 0]]];
   const constrainedDelta = constrainFeatureMovement([polygon], {
     lat: 13,
     lng: 0
@@ -187,7 +187,7 @@ test('constrainFeatureMovement polygon, no constraint', () => {
 
 test('constrainFeatureMovement polygon, requiring northern inner constraint', () => {
   const polygon = getGeoJSON('polygon');
-  polygon.geometry.coordinates = [[80, 80], [81, 81], [81, 82], [80, 80]];
+  polygon.geometry.coordinates = [[[80, 80], [81, 81], [81, 82], [80, 80]]];
   const constrainedDelta = constrainFeatureMovement([polygon], {
     lat: 7,
     lng: 0
@@ -201,7 +201,7 @@ test('constrainFeatureMovement polygon, requiring northern inner constraint', ()
 
 test('constrainFeatureMovement polygon, requiring northern outer constraint', () => {
   const polygon = getGeoJSON('polygon');
-  polygon.geometry.coordinates = [[30, 30], [30, 40], [81, 81], [30, 30]];
+  polygon.geometry.coordinates = [[[30, 30], [30, 40], [81, 81], [30, 30]]];
   const constrainedDelta = constrainFeatureMovement([polygon], {
     lat: 12,
     lng: 0
@@ -215,7 +215,7 @@ test('constrainFeatureMovement polygon, requiring northern outer constraint', ()
 
 test('constrainFeatureMovement polygon, requiring southern inner constraint', () => {
   const polygon = getGeoJSON('polygon');
-  polygon.geometry.coordinates = [[-80, -80], [-81, -81], [-81, -82], [-80, -80]];
+  polygon.geometry.coordinates = [[[-80, -80], [-81, -81], [-81, -82], [-80, -80]]];
   const constrainedDelta = constrainFeatureMovement([polygon], {
     lat: -7,
     lng: 0
@@ -229,7 +229,7 @@ test('constrainFeatureMovement polygon, requiring southern inner constraint', ()
 
 test('constrainFeatureMovement polygon, requiring southern outer constraint', () => {
   const polygon = getGeoJSON('polygon');
-  polygon.geometry.coordinates = [[-30, -30], [-30, -40], [-81, -81], [-30, -30]];
+  polygon.geometry.coordinates = [[[-30, -30], [-30, -40], [-81, -81], [-30, -30]]];
   const constrainedDelta = constrainFeatureMovement([polygon], {
     lat: -12,
     lng: 0
@@ -243,7 +243,7 @@ test('constrainFeatureMovement polygon, requiring southern outer constraint', ()
 
 test('constrainFeatureMovement polygon, requiring western wrap', () => {
   const polygon = getGeoJSON('polygon');
-  polygon.geometry.coordinates = [[0, 0], [10, 10], [20, 20], [0, 0]];
+  polygon.geometry.coordinates = [[[0, 0], [10, 10], [20, 20], [0, 0]]];
   const constrainedDelta = constrainFeatureMovement([polygon], {
     lat: 70,
     lng: -270
@@ -257,7 +257,7 @@ test('constrainFeatureMovement polygon, requiring western wrap', () => {
 
 test('constrainFeatureMovement polygon, requiring eastern wrap', () => {
   const polygon = getGeoJSON('polygon');
-  polygon.geometry.coordinates = [[0, 0], [10, 10], [20, 20], [0, 0]];
+  polygon.geometry.coordinates = [[[0, 0], [10, 10], [20, 20], [0, 0]]];
   const constrainedDelta = constrainFeatureMovement([polygon], {
     lat: 35,
     lng: 270
@@ -271,7 +271,7 @@ test('constrainFeatureMovement polygon, requiring eastern wrap', () => {
 
 test('constrainFeatureMovement many features, no constraint', () => {
   const polygon = getGeoJSON('polygon');
-  polygon.geometry.coordinates = [[0, 0], [10, 10], [20, 20], [0, 0]];
+  polygon.geometry.coordinates = [[[0, 0], [10, 10], [20, 20], [0, 0]]];
   const point = getGeoJSON('point');
   point.geometry.coordinates = [15, 15];
   const line = getGeoJSON('line');
@@ -289,7 +289,7 @@ test('constrainFeatureMovement many features, no constraint', () => {
 
 test('constrainFeatureMovement many features, requiring northern inner constraint', () => {
   const polygon = getGeoJSON('polygon');
-  polygon.geometry.coordinates = [[80, 80], [81, 81], [81, 82], [80, 80]];
+  polygon.geometry.coordinates = [[[80, 80], [81, 81], [81, 82], [80, 80]]];
   const point = getGeoJSON('point');
   point.geometry.coordinates = [15, 15];
   const line = getGeoJSON('line');
@@ -307,7 +307,7 @@ test('constrainFeatureMovement many features, requiring northern inner constrain
 
 test('constrainFeatureMovement many features, requiring northern outer constraint', () => {
   const polygon = getGeoJSON('polygon');
-  polygon.geometry.coordinates = [[0, 0], [10, 10], [20, 20], [0, 0]];
+  polygon.geometry.coordinates = [[[0, 0], [10, 10], [20, 20], [0, 0]]];
   const point = getGeoJSON('point');
   point.geometry.coordinates = [15, 15];
   const line = getGeoJSON('line');
@@ -325,7 +325,7 @@ test('constrainFeatureMovement many features, requiring northern outer constrain
 
 test('constrainFeatureMovement many features, requiring southern inner constraint', () => {
   const polygon = getGeoJSON('polygon');
-  polygon.geometry.coordinates = [[-80, -80], [-81, -81], [-81, -82], [-80, -80]];
+  polygon.geometry.coordinates = [[[-80, -80], [-81, -81], [-81, -82], [-80, -80]]];
   const point = getGeoJSON('point');
   point.geometry.coordinates = [15, 15];
   const line = getGeoJSON('line');
@@ -343,7 +343,7 @@ test('constrainFeatureMovement many features, requiring southern inner constrain
 
 test('constrainFeatureMovement many features, requiring southern outer constraint', () => {
   const polygon = getGeoJSON('polygon');
-  polygon.geometry.coordinates = [[0, 0], [10, 10], [20, 20], [0, 0]];
+  polygon.geometry.coordinates = [[[0, 0], [10, 10], [20, 20], [0, 0]]];
   const point = getGeoJSON('point');
   point.geometry.coordinates = [15, 15];
   const line = getGeoJSON('line');
@@ -361,7 +361,7 @@ test('constrainFeatureMovement many features, requiring southern outer constrain
 
 test('constrainFeatureMovement many features, requiring western wrap', () => {
   const polygon = getGeoJSON('polygon');
-  polygon.geometry.coordinates = [[0, 0], [10, 10], [20, 20], [0, 0]];
+  polygon.geometry.coordinates = [[[0, 0], [10, 10], [20, 20], [0, 0]]];
   const point = getGeoJSON('point');
   point.geometry.coordinates = [15, 15];
   const line = getGeoJSON('line');
@@ -379,7 +379,7 @@ test('constrainFeatureMovement many features, requiring western wrap', () => {
 
 test('constrainFeatureMovement many features, requiring eastern wrap', () => {
   const polygon = getGeoJSON('polygon');
-  polygon.geometry.coordinates = [[0, 0], [10, 10], [20, 20], [0, 0]];
+  polygon.geometry.coordinates = [[[0, 0], [10, 10], [20, 20], [0, 0]]];
   const point = getGeoJSON('point');
   point.geometry.coordinates = [15, 15];
   const line = getGeoJSON('line');
