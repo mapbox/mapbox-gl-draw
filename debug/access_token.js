7461713de2892eac166a360d5bd01f83f7972c52
Paulius Varna <paul.varna@gmail.com>
2024-07-03 13:30:05 +0300

    chore: Update Mapbox access token variable name

--- debug/access_token.js
+++ debug/access_token.js
@@ -1,6 +1,6 @@
 export function getAccessToken() {
   const accessToken =
-    import.meta.env.VITE_MAPBOX_ACCESS_TOKEN ||
+    import.meta.env.MAPBOX_ACCESS_TOKEN ||
     (new URLSearchParams(location.search).get('access_token')) ||
     localStorage.getItem('accessToken');
 
