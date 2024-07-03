41d736a69ed4ee98ffaa41080fe61b7d7ee52d2c
Paulius Varna <paul.varna@gmail.com>
2024-07-03 13:28:44 +0300

    chore: Use rollup within vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [eslint()],
-  root: 'debug/',
-  base: '/debug/',
   envPrefix: 'MAPBOX_',
   server: {
     host: '0.0.0.0',
@@ -18,5 +17,21 @@ export default defineConfig({
         global: 'globalThis'
       }
     }
+  },
+  build: {
+    sourcemap: true,
+    minify: 'terser',
+    lib: {
+      entry: resolve(__dirname, 'index.js'),
+      name: 'MapboxDraw',
+      fileName: (format) => {
+        switch (format) {
+        case 'es':
+          return 'mapbox-gl-draw-unminified.js';
+        case 'umd':
+          return 'mapbox-gl-draw.js';
+        }
+      }
+    },
   }
 });
