3a1b15411b4e9a97ab2ede31f743288718fc627e
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2024-07-30 17:06:10 +0300

    cleanup

--- src/store.js
+++ src/store.js
@@ -321,7 +321,7 @@ Store.prototype.setFeatureProperty = function(featureId, property, value) {
 function refreshSelectedCoordinates(store, options) {
   const newSelectedCoordinates = store._selectedCoordinates.filter(point => store._selectedFeatureIds.has(point.feature_id));
   if (store._selectedCoordinates.length !== newSelectedCoordinates.length && !options.silent) {
-    store.emitSelectionChange();
+    store._emitSelectionChange = true;
   }
   store._selectedCoordinates = newSelectedCoordinates;
 }
