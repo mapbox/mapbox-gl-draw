5b71f7b630dc341c9d595203d29d4b491f82a4d5
tristen <tristen.brown@gmail.com>
2025-03-06 14:23:46 +0200

    Simple typings for common selectors

--- src/lib/common_selectors.ts
+++ src/lib/common_selectors.ts
@@ -1,7 +1,9 @@
 import * as Constants from '../constants';
+import { MapMouseEvent, MapTouchEvent } from '../types/types';
 
-export function isOfMetaType(type) {
-  return function (e) {
+type E = MapMouseEvent | MapTouchEvent;
+
+export function isOfMetaType(type: string) { return function (e: E) {
     const featureTarget = e.featureTarget;
     if (!featureTarget) return false;
     if (!featureTarget.properties) return false;
@@ -9,13 +11,13 @@ export function isOfMetaType(type) {
   };
 }
 
-export function isShiftMousedown(e) {
+export function isShiftMousedown(e: MapMouseEvent) {
   if (!e.originalEvent) return false;
   if (!e.originalEvent.shiftKey) return false;
   return e.originalEvent.button === 0;
 }
 
-export function isActiveFeature(e) {
+export function isActiveFeature(e: E) {
   if (!e.featureTarget) return false;
   if (!e.featureTarget.properties) return false;
   return (
@@ -24,7 +26,7 @@ export function isActiveFeature(e) {
   );
 }
 
-export function isInactiveFeature(e) {
+export function isInactiveFeature(e: E) {
   if (!e.featureTarget) return false;
   if (!e.featureTarget.properties) return false;
   return (
@@ -33,34 +35,34 @@ export function isInactiveFeature(e) {
   );
 }
 
-export function noTarget(e) {
+export function noTarget(e: E) {
   return e.featureTarget === undefined;
 }
 
-export function isFeature(e) {
+export function isFeature(e: E) {
   if (!e.featureTarget) return false;
   if (!e.featureTarget.properties) return false;
   return e.featureTarget.properties.meta === Constants.meta.FEATURE;
 }
 
-export function isVertex(e) {
+export function isVertex(e: E) {
   const featureTarget = e.featureTarget;
   if (!featureTarget) return false;
   if (!featureTarget.properties) return false;
   return featureTarget.properties.meta === Constants.meta.VERTEX;
 }
 
-export function isShiftDown(e) {
+export function isShiftDown(e: MapMouseEvent) {
   if (!e.originalEvent) return false;
   return e.originalEvent.shiftKey === true;
 }
 
-export function isEscapeKey(e) {
-  return e.keyCode === 27;
+export function isEscapeKey(e: KeyboardEvent) {
+  return e.key === 'Escape';
 }
 
-export function isEnterKey(e) {
-  return e.keyCode === 13;
+export function isEnterKey(e: KeyboardEvent) {
+  return e.key === 'Enter';
 }
 
 export function isTrue() {
