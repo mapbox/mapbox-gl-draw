bd51259f6e1c1f4c0e1428faff6dd2a505981ad3
tristen <tristen.brown@gmail.com>
2025-03-17 18:38:57 -0400

    Fix rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,27 @@
+import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
+
 const { MINIFY } = process.env;
 const minified = MINIFY === 'true';
 const outputFile = minified
   ? 'dist/mapbox-gl-draw.js'
   : 'dist/mapbox-gl-draw-unminified.js';
 
-import typescript from '@rollup/plugin-typescript';
-
 export default {
   input: 'index.ts',
   output: {
     name: 'MapboxDraw',
     file: outputFile,
-    format: 'esm', // Ensure ESM output if using ES modules
-    sourcemap: true
+    format: 'esm',
+    sourcemap: true,
+    indent: false
   },
-  plugins: [typescript()]
+  treeshake: true,
+  plugins: [
+    typescript(),
+    minified ? terser({
+      ecma: 2020,
+      module: true,
+    }) : false,
+  ]
 };
